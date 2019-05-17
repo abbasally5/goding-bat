52aeca47fd0a19008d2a1aa430a8356893d17dbb
Abbas Ally <abbasally5@yahoo.com>
2019-05-17 18:06:52 -0500

    Now serving inherited templates - Also serving static files

--- app.go
+++ app.go
@@ -6,5 +6,32 @@ import (
 	"net/http"
 )
 
+// Handlers
+var baseTemplates = []string{
+	"templates/problem_sets.tmpl",
+	"templates/layout/footer.tmpl",
+	"templates/layout/header.tmpl",
+	"templates/layout/base.tmpl",
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(baseTemplates...)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = t.ExecuteTemplate(w, "problem_sets.tmpl", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", homeHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
